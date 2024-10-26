3194271dce5104e51aef887726851b515c9e0f12
Pdorla <Pdorla@qq.com>
2024-10-27 05:06:25 +0800

    修改启动脚本

--- start-api-release.bat
+++ start-api-release.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: 配置端口
-set API_PORT=8080
+set API_PORT=8081
 set API_DIR=".\slim-api\public"
 
 :: 启动 PHP 内置服务器
--- start-api-release.sh
+++ start-api-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 配置端口、目录
-API_PORT=8080
+API_PORT=8081
 API_DIR="./slim-api/public"
 
 # 启动 PHP 内置服务器
